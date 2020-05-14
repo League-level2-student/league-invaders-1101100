6c0c3be454aa37151d09cfc040620657a444c4b8
1101100 <joshtang255@gmail.com>
2020-05-13 19:53:12 -0700

    started league invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,26 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	static int width = 500;
+	static int height = 800;
+	GamePanel gPanel = new GamePanel();
+	
+	JFrame frame = new JFrame();
+	
+	
+	public static void main(String[] args) {
+		LeagueInvaders game = new LeagueInvaders();
+		
+	}
+	void setup() {
+		frame.add(gPanel);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setPreferredSize(new Dimension(width, height));
+		frame.pack();
+		
+	}
+	
 }
